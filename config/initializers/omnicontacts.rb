b0f923e70fffae3e466ae8b0defc65e9ec37aea9
vincentbwalker <vincethewalker@gmail.com>
2018-11-15 22:27:24 +0000

    configured omnicontacts file for correct redirectto gmail as well a modified the Routes file to enable this

--- config/initializers/omnicontacts.rb
+++ config/initializers/omnicontacts.rb
@@ -1,6 +1,6 @@
 require "omnicontacts"
 
 Rails.application.middleware.use OmniContacts::Builder do
-  importer :gmail, "961427654088-frpmnarj4i4qfs8dgeatk40hkl11o5p6.apps.googleusercontent.com", "LI5Wz3HipvpLZIVyVKFOqELb", {:redirect_path => "/oauth2callback", :ssl_ca_file => "/etc/ssl/certs/curl-ca-bundle.crt"}
+  importer :gmail, "961427654088-frpmnarj4i4qfs8dgeatk40hkl11o5p6.apps.googleusercontent.com", "LI5Wz3HipvpLZIVyVKFOqELb", {:redirect_path => "/contacts/gmail/contact_callback"}
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  root 'contacts#index'
-  #get '/index' => 'pages#index'
-  #get '/auth/:provider/callback', to: 'sessions#create'
-  #get 'auth/facebook/callback', to: 'sessions#create'
-  #get 'auth/failure', to: redirect('/')
-  
-  #get 'auth/google/callback', to: 'sessions#create'
-  #get 'auth/failure', to: redirect('/')
+    get "/contacts/:provider/contact_callback" => "contacts#index"
+    get "/contacts/failure" => "contacts#failure"
+    root 'contacts#index'
 end
