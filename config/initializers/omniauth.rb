0f4f8ed90877d0985845d93a1f66b8120c23166c
vincentbwalker <vincethewalker@gmail.com>
2018-11-15 20:51:07 +0000

    Created Contacts folder and Index file +  contacts and created initializers/omnicontacts.rb to configure gmail

--- config/initializers/omniauth.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, "0GdCPW5mIneMMvAN1ukxS9LkM", "c16qRgpuVUjqLifajBJgbsXEbmNJDp6cHGFybs4m9pptS07r2j"
-  provider :facebook, Rails.application.secrets.facebook_app_id
-  #Rails.application.secrets.facebook_app_secret, scope: ‘public_profile’, info_fields: ‘id,name,link’
-  provider :google_oauth2, Rails.application.secrets.google_client_id
-  #Rails.application.secrets.google_client_secret, scope: ‘public_profile’, info_fields: ‘id,name,link’
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
-  get '/index' => 'pages#index'
-  get '/auth/:provider/callback', to: 'sessions#create'
-  get 'auth/facebook/callback', to: 'sessions#create'
-  get 'auth/failure', to: redirect('/')
+  root 'contacts#index'
+  #get '/index' => 'pages#index'
+  #get '/auth/:provider/callback', to: 'sessions#create'
+  #get 'auth/facebook/callback', to: 'sessions#create'
+  #get 'auth/failure', to: redirect('/')
   
-  get 'auth/google/callback', to: 'sessions#create'
-  get 'auth/failure', to: redirect('/')
+  #get 'auth/google/callback', to: 'sessions#create'
+  #get 'auth/failure', to: redirect('/')
 end
