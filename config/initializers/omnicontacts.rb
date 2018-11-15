7985d3fd6d66aa3b9f712fefb5c5827b6e871d56
vincentbwalker <vincethewalker@gmail.com>
2018-11-15 22:13:37 +0000

    Added Keys again!

--- config/initializers/omnicontacts.rb
+++ config/initializers/omnicontacts.rb
@@ -1,6 +1,6 @@
 require "omnicontacts"
 
 Rails.application.middleware.use OmniContacts::Builder do
-  importer :gmail, "client_id", "client_secret", {:redirect_path => "/oauth2callback", :ssl_ca_file => "/etc/ssl/certs/curl-ca-bundle.crt"}
+  importer :gmail, "961427654088-frpmnarj4i4qfs8dgeatk40hkl11o5p6.apps.googleusercontent.com", "LI5Wz3HipvpLZIVyVKFOqELb", {:redirect_path => "/oauth2callback", :ssl_ca_file => "/etc/ssl/certs/curl-ca-bundle.crt"}
   
 end
