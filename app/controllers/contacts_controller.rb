e05bd8e82b011fce64f13bbf3a6dadfc8b8abb07
vincentbwalker <vincethewalker@gmail.com>
2018-11-16 01:24:59 +0000

    final clean up

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,7 +2,7 @@ class ContactsController < ApplicationController
     
     
     def index
-        @contacts = request.env['omnicontacts.contacts']
+        @contacts = request.env['omnicontacts.contacts'] 
             respond_to do |format|
                 format.html
             end
